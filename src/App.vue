3aa88deae0bca2f68d6df788d7610babe04a3a3d
Guterresthomas <guterresmikael@gmail.com>
2023-10-11 22:55:33 -0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloMessage.vue';
 import ProductGrid from './components/ProductGrid.vue';
-import Footer from './components/footer.vue'
+
 import Banner from './components/Banner.vue';
 import TextDavines from './components/TextDavines.vue'
 </script>
@@ -25,7 +25,9 @@ import TextDavines from './components/TextDavines.vue'
     <TextDavines/>
   </div>
   <footer>
-    <Footer />
+      <div class="footer-content">
+        <p>Copyright © 2023 Gabtai.com.br. Todos os direitos reservados.</p>
+      </div>
   </footer>
 </template>
 
@@ -91,4 +93,45 @@ header {
 }
 
 }
+
+footer {
+    background-color: rgb(4, 77, 137);
+    color: white;
+    padding: 20px 0;
+    text-align: center;
+    margin-bottom: 0;
+    
+    width: 100%;
+    display: flex;
+  }
+  
+  .footer-content {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .footer-content p {
+    margin: 0;
+    font-size: 16px;
+  }
+  
+  .footer-content ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  .footer-content li {
+    display: inline;
+    margin-right: 20px;
+  }
+  
+  .footer-content a {
+    color: #fff;
+    text-decoration: none;
+    font-size: 16px;
+  }
 </style>
